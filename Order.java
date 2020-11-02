c53e3ec299e5a16780bd9464a6d3405672a9349b
zakiyahwalker <37126731+zakiyahwalker@users.noreply.github.com>
2020-11-02 16:41:47 -0500

    Create Order.java

---
+++ Order.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Order {
+    private String ordProd;
+    private double ordPrice;
+
+    public String getOrdProd() {
+        return ordProd;
+    }
+
+    public void setOrdProd(String ordProd) {
+        this.ordProd = ordProd;
+    }
+
+    public double getOrdPrice() {
+        return ordPrice;
+    }
+
+    public void setOrdPrice(double ordPrice) {
+        this.ordPrice = ordPrice;
+    }
+}
