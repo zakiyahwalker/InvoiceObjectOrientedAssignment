bec24d0a87313dc2d795ec8af7c963ff7aa3477c
zakiyahwalker <37126731+zakiyahwalker@users.noreply.github.com>
2020-11-02 16:42:15 -0500

    Create Store.java

---
+++ Store.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Store {
+
+    private String products;
+   private double prices;
+
+    public String getProducts() {
+        return products;
+    }
+
+    public void setProducts(String products) {
+        this.products = products;
+    }
+
+    public double getPrices() {
+        return prices;
+    }
+
+    public void setPrices(double prices) {
+        this.prices = prices;
+    }
+}
